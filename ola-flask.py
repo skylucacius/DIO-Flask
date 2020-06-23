e83917e8d044b6e4414affa5ad8432229ce3ee94
skylucacius <skylucacius@hotmail.com>
2020-06-23 16:45:58 -0300

    virtual env separado

--- ola-flask.py
+++ ola-flask.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+import json
+
 app = Flask(__name__)
 
 @app.route('/numero/<numero>')
@@ -8,6 +10,21 @@ def mostra_numero(numero):
 @app.route('/', methods = [ 'get', 'post'])
 def rota_padrao():
     return {'nome':'lucas','profissão':'estudante'}
+
+@app.route('/soma', methods=['get','post'])
+def somar():
+    # Também pode ser feito da seguinte forma
+    # dados = request.get_json(force=True)
+    # return str(sum(dados['valores']))
+
+    if request.method == 'post':
+        dados = json.loads(request.data)
+        soma = sum(dados['valores'])
+    else:
+        soma = 10 + 10
+    return {'total': soma}
+    
+
 
 if __name__ == '__main__':
     app.run()
